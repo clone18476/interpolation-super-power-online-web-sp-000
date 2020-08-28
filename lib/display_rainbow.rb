f7e3292e7d83473c751d49321ff87c86d12cd9ec
Cameron Cousins <cameroncousins18@gmail.com>
2020-08-28 15:03:22 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,6 @@
 # Write your #display_rainbow method here
+
+
+def display_rainbow(color="red, orange, yellow, green, blue, indigo, violet)
+  puts "#{color[0]}, #{color[1]}, #{color[2]}, #{color[3]}, #{color[4]}, #{color[5]}, #{color[6]}"
+end 
